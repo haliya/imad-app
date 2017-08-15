93d4f29aff248da48c81bf086a21c7d3d8a414e3
haliya <30822013+haliya@users.noreply.github.com>
2017-08-15 17:09:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,90 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var article one = {
+    title: 'Article one',
+    heading: 'Article one',
+    content:
+              <p>
+                  this is the content of my first article.this is the content of my first 
+                      article.this is the content of my first article.this is the content of 
+                      my first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article
+                      .this is the content of my first article.this is the content of my 
+                      first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article
+                      .this is the content of my first article.this is the content of my 
+                      first article.
+              </p>
+              <p>
+                  this is the content of my first article.this is the content of my first 
+                      article.this is the content of my first article.this is the content of 
+                      my first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article
+                      .this is the content of my first article.this is the content of my 
+                      first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article
+                      .this is the content of my first article.this is the content of my 
+                      first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article
+                      .this is the content of my first article.this is the content of my 
+                      first article.this is the content of my first article.this is the 
+                      content of my first article.this is the content of my first article.
+              </p>
+         
+    
+    
+    
+    
+};
+
+function createTemplate [data]={
+    var title= data.title;
+    var heading =data.heading;
+    var content = data.content;
+
+var htmlTemplate= '
+    
+    <html>
+    <head>
+        <title>
+            $[title]
+        </title>
+   
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class='container'>
+            
+        <div>
+            <a href='/'>Home</a>
+            <hr/>
+        </div>
+        <div>
+            <h3>
+                $[heading]
+            </h3>
+        </div>
+          <div>
+              $[content]
+          </div>
+        </div>  
+        
+    </body>
+  </html>
+
+      ';
+       return htmlTemplate;
+    
+    
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){ 
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.send(createTemplate(article one));
     
 });
 
