ee11f22d33b7ea52d290b6ad5b07ec7a22617f47
haliya <30822013+haliya@users.noreply.github.com>
2017-08-17 16:35:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,15 @@ element.innerHTML='New value';
 
 // move the image
 var img =document.getElementById('madi');
+var marginLeft=0;
+function moveRight (){
+     marginLeft= marginLeft + 10;
+     img.style.maginLeft=marginLeft+ 'px';
+}
+    
+
+
 img.onclick = function () {
-    var interval= setinterval('marginLeft,100')
-    img.style.marginLeft='100px';
+    var interval= setInterval('moveRight, 100');
+    
 };
