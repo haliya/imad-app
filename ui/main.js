f80ea070458435023eb9fd98ac216c006d04cca9
haliya <30822013+haliya@users.noreply.github.com>
2017-08-17 17:01:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,12 +12,13 @@ var img =document.getElementById('madi');
 //var marginLeft=0;
 //function moveRight (){
     // marginLeft= marginLeft + 10;
-     img.style.maginLeft= '100px';
-}
+    //img.style.marginLeft=marginLeft+ 'px';
+    //}
     
 
 
 img.onclick = function () {
-   // var interval= setInterval(moveRight, 100);
     
+         img.style.maginLeft= '100px';
+
 };
