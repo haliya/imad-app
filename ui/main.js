1dd8f32f29b57172d7dfc112bb9ded44b86dfb3d
haliya <30822013+haliya@users.noreply.github.com>
2017-08-17 13:36:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the content
-var elament =document.getElementbyId('main-text');
-elament.innerHtml='new value';
+var element =document.getElementbyId('main-text');
+element.innerHTML='new value';
