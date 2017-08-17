cb93e02aa0a0de182cf31af1d19aaa754a054905
haliya <30822013+haliya@users.noreply.github.com>
2017-08-17 16:58:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,15 +9,15 @@ element.innerHTML='New value';
 
 // move the image
 var img =document.getElementById('madi');
-var marginLeft=0;
-function moveRight (){
-     marginLeft= marginLeft + 10;
-     img.style.maginLeft=marginLeft+ 'px';
+//var marginLeft=0;
+//function moveRight (){
+    // marginLeft= marginLeft + 10;
+     img.style.maginLeft= '100px';
 }
     
 
 
 img.onclick = function () {
-    var interval= setInterval(moveRight, 100);
+   // var interval= setInterval(moveRight, 100);
     
 };
