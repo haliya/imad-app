1eb46419d8780decbb28bce16e8c9096a43cc26d
haliya <30822013+haliya@users.noreply.github.com>
2017-08-20 16:34:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,6 +112,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function (req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+
 app.get('/:articlename', function (req, res){ 
 var articlename=req.params.articlename;
 res.send(createTemplate(articles[articlename]));
@@ -134,11 +141,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
-});
-var counter=0;
-app.get('/counter', function (req, res){
-    counter=counter+1;
-    res.send(counter.toString());
 });
 
 
