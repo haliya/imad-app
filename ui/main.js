a2979c05dc57d51b339544206319b1e804fa22de
haliya <30822013+haliya@users.noreply.github.com>
2017-08-20 20:24:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,8 +12,8 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function (){
     if(request.readyState=== XMLHttpRequest.DONE){
         // take some action
-        if(request.status== 200){
-            var counter =request.responeText;
+        if(request.status=== 200){
+            var counter =request.responseText;
             var span = document.getElementById('count');
                span.innerHTML = counter.toString();
 
